aeb99a5714da4f6cb0d61619a4553cc213f4e46b
Arturo_LM <artlman410@gmail.com>
2025-07-25 12:39:32 -0500

    primer_avace

---
+++ script.js
@@ -0,0 +1,21 @@
+// Galería dinámica
+const galeria = [
+  'https://images.app.goo.gl/vecDA',
+  'https://via.placeholder.com/400x300?text=Proyecto+2',
+  'https://via.placeholder.com/400x300?text=Proyecto+3'
+];
+
+const galeriaContainer = document.getElementById('galeria');
+
+galeria.forEach(img => {
+  const div = document.createElement('div');
+  div.className = 'col-md-4';
+  div.innerHTML = `<img src="${img}" class="galeria-img" alt="Proyecto">`;
+  galeriaContainer.appendChild(div);
+});
+
+// Formulario (simulación de envío)
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Formulario enviado. ¡Gracias por contactarnos!');
+});
